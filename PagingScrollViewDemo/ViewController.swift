5542a7b0fc633bda8b903d70d1403321613ca765
Jiro Nagashima <hedjirog@gmail.com>
2014-12-14 15:27:39 +0900

    Add project files

---
+++ PagingScrollViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PagingScrollViewDemo
+//
+//  Created by Jiro Nagashima on 12/14/14.
+//  Copyright (c) 2014 Jiro Nagashima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
